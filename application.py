c2648196cd7e1626ecbc019b91b7ab57357dc377
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2024-03-04 20:13:41 +0000

    Mise en place de mon application Streamlit

---
+++ application.py
@@ -0,0 +1,15 @@
+# import des bibliothèques streamlit et pandas
+import streamlit as st
+import pandas as pd
+
+# write, fonction pour écrire un message sur la page
+st.write("""
+# Ma première application avec Streamlit
+Salut **DIT!** Comment allez vous ?
+""")
+
+# récupération d'un Dataframe à partir d'un fichier Excel
+df = pd.read_excel("patients_list.xlsx")
+
+# tracé d'une courbe
+# st.line_chart(df)
